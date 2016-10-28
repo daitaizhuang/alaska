12604d65680af1fab2ad0f745c5516e813867beb
lockllb <lockllb@lockllbdeMac-mini.local>
2016-10-28 11:49:00 +0800

    fire

--- src/Thomas/Gun/Gun2016.php
+++ src/Thomas/Gun/Gun2016.php
@@ -1,12 +1,8 @@
 <?php
 namespace Thomas\Gun;
-<<<<<<< HEAD
-=======
-
->>>>>>> 66109965099c85e02845cf1447211066079750a3
 class Gun2016{
 	public function say()
 	{
-		echo "yes....".time();
+		echo "fire::";
 	}
 }
